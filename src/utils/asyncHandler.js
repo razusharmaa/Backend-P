2c4049775e0cda004311d4284a0ba205e7c789c0
Raju Sharma <113360219+razusharmaa@users.noreply.github.com>
2024-05-14 15:46:31 +0545

    utils complete

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,33 @@
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+/*
+const asyncHandler=()=>{}
+const asyncHandler=(fun)=>{()=>{}}
+const asyncHandler=()=>()=>{}
+
+
+const asyncHandler= (fn)=> async (req,res,next)=>{
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        res.status(error.status || 500).json({
+            success: false,
+            message:error.message
+        })
+        
+    }
+}
+*/
